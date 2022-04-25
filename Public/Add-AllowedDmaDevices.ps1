88298435d9f8a1100084b65823c929ad84255141
Jason Cook <JasonCook599@users.noreply.github.com>
2022-04-25 12:40:00 -0400

    Fix from bad autoformat

--- Public/Add-AllowedDmaDevices.ps1
+++ Public/Add-AllowedDmaDevices.ps1
@@ -1,53 +1,7 @@
 <#PSScriptInfo
-.VERSION 1.0.0
-.GUID a684ddd1-559b-48e2-bbdf-a85a3d50d3f6
 
-.AUTHOR
-Jason Cook
+.VERSION 1.0.1
 
-.COMPANYNAME
-***REMOVED***
-
-.COPYRIGHT
-Copyright (c) ***REMOVED*** 2022
-#>
-
-<#
-.SYNOPSIS
-This script will Allow spesific devices to the list of Allowed Buses.
-
-.DESCRIPTION
-This script will Allow spesific devices to the list of Allowed Buses. The primary use if for automatic BitLocker encryption.
-
-.PARAMETER ComputerInfo
-The Manufacturer and Model of the current device.
-
-.PARAMETER Path
-The registry path for AllowedBuses
-
-.PARAMETER DeviceList
-A hashtable of all the address of allowed devices in the format of Manufactuer.Model.Name.
-
-.PARAMETER AllowedDevices
-An list of all the devices allowed on this spesific device.
-#>
-param(
-    $ComputerInfo = (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Manufacturer, Model),
-    $Path = "HKLM:\SYSTEM\CurrentControlSet\Control\DmaSecurity\AllowedBuses",
-    $DeviceList = @{
-        Lenovo = @{
-            "20YG003FUS" = @{
-                "PCI Express Root Port A" = "PCI\VEN_1022&DEV_1634"
-                "PCI Express Root Port B" = "PCI\VEN_1022&DEV_1635"
-                "PCI standard ISA bridge" = "PCI\VEN_1022&DEV_790E"
-            }
-        }
-    },
-    $AllowedDevices = $DeviceList.$($ComputerInfo.Manufacturer).$($ComputerInfo.Model)
-)
-foreach ($Device in $AllowedDevices.GetEnumerator()) {
-    New-ItemProperty -Path $Path -Name $Device.Name -Value $Device.Value -PropertyType "String" -Force -WhatIf
-}
 .GUID a684ddd1-559b-48e2-bbdf-a85a3d50d3f6
 
 .AUTHOR Jason Cook
@@ -74,12 +28,6 @@ foreach ($Device in $AllowedDevices.GetEnumerator()) {
 
 
 #> 
-
-
-
-
-
-
 
 
 
