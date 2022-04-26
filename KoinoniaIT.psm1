fb6874719eb137ad7a2535db74c22d883ef7dcde
Jason Cook <JasonCook599@users.noreply.github.com>
2022-04-26 11:29:44 -0400

    Add Get-GroupMembershipReport

--- KoinoniaIT.psm1
+++ KoinoniaIT.psm1
@@ -2741,6 +2741,68 @@ Function Get-BiosType {
     [FirmwareType]::GetFirmwareType()
 }
 }
+function Get-GroupMembershipReport {
+<#PSScriptInfo
+
+.VERSION 1.0.1
+
+.GUID b2ff192c-1106-4c52-ab8c-b7cab4524cc9
+
+.AUTHOR Jason Cook
+
+.COMPANYNAME ***REMOVED***
+
+.COPYRIGHT Copyright (c) ***REMOVED*** 2022
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#> 
+
+
+
+<#
+.DESCRIPTION
+Gets group membership information for the specified groups.
+
+.PARAMETER Filter
+Filters the search based on the spesified parameters.
+
+.PARAMETER SearchBase
+The LDAP search base.
+#>
+
+param (
+    $Filter = "*",
+    $SearchBase
+)
+$Results = @()
+Get-ADGroup -SearchBase $SearchBase -Filter * -Properties Description | ForEach-Object {
+    $MembersString = (Get-ADGroupMember -Identity $_.DistinguishedName).Name -join ";"
+    $Result = [PSCustomObject]@{
+        Name          = $_.Name
+        Description   = $_.Description
+        MembersString = $MembersString
+        Members       = (Get-ADGroupMember -Identity $_.DistinguishedName).Name
+    }
+    $Results += $Result    
+}
+return $Results
+}
 function Get-ipPhone {
 <#PSScriptInfo
 
