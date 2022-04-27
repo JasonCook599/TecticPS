24cf752cea58adfd608fae7ae6ab841c3bd58bb6
Jason Cook <JasonCook599@users.noreply.github.com>
2022-04-27 09:59:41 -0400

    Add Set-RoomCalendarPermissions

---
+++ Public/Set-RoomCalendarPermissions.ps1
@@ -0,0 +1,57 @@
+<#PSScriptInfo
+
+.VERSION 1.0.3
+
+.GUID 9d477618-5530-413c-bdf8-3ddf1580dbfa
+
+.AUTHOR Jason Cook
+
+.COMPANYNAME ***REMOVED***
+
+.COPYRIGHT Copyright (c) ***REMOVED*** 2022
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#> 
+
+
+
+
+
+<#
+.DESCRIPTION
+Makes Availability information available to all users.
+
+.PARAMETER User
+What user should the permissions be set for. If not specified, the DEFAULT user is used.
+
+.PARAMETER AccessRight
+The access right to set. By default, the access right is set to LimitedDetails.
+#>
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+    $User = "Default",
+    $AccessRights = "LimitedDetails"
+)
+
+Get-Mailbox -RecipientTypeDetails RoomMailbox | ForEach-Object {
+    If ($PSCmdlet.ShouldProcess("$_", "Set-RoomCalendarPermissions")) {
+        Set-MailboxFolderPermission -Identity $($_.Identity + ":\Calendar") -User $User -AccessRights $AccessRights
+    }
+}
+
