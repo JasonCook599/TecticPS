90be1267307f35b473fa8a0c1634321f0c712633
Jason Cook <JasonCook599@users.noreply.github.com>
2022-04-28 13:29:05 -0400

    Add new functions

---
+++ Public/Get-AzureAdMfaStatus.ps1
@@ -0,0 +1,58 @@
+<#PSScriptInfo
+
+.VERSION 1.0.3
+
+.GUID 036c4b38-9023-4f7b-9254-e8d7683f56e2
+
+.AUTHOR Jason Cook
+
+.COMPANYNAME ***REMOVED***
+
+.COPYRIGHT Copyright (c) ***REMOVED*** 2022
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#> 
+
+
+
+<#
+.DESCRIPTION
+The script will get information about MFA setup from Azure Active Directory.
+
+.PARAMETER Filter
+Filters the AAD query based on the spesified parameters.
+
+.PARAMETER WhereObject
+Filters the returned results based on the spesified parameters.
+
+.PARAMETER Properties
+The properties to return from the search.
+
+.PARAMETER SortKey
+The sort key to use when sorting the results. By default, this is the first property selected.
+#>
+
+param(
+    [string]$Filter,
+    $Properties = @("UserPrincipalName", "DisplayName", "FirstName", "LastName", @{N = "MFA Status"; E = { if ( $_.StrongAuthenticationRequirements.State -ne $null) { $_.StrongAuthenticationRequirements.State } else { "Disabled" } } }),
+    $SortKey = $Properties[0]
+)
+try { . (LoadDefaults -Invocation $MyInvocation) -Invocation $MyInvocation } catch { Write-Warning "Failed to load defaults. Is the module loaded?" }
+
+return Get-MsolUser -All  | Sort-Object $SortKey | Select-Object $Properties
---
+++ Public/Get-AzureAdUserInfo.ps1
@@ -0,0 +1,59 @@
+<#PSScriptInfo
+
+.VERSION 1.0.3
+
+.GUID 3af068df-1f2d-4e6b-b1a7-e18e09311471
+
+.AUTHOR Jason Cook
+
+.COMPANYNAME ***REMOVED***
+
+.COPYRIGHT Copyright (c) ***REMOVED*** 2022
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#> 
+
+
+
+<#
+.DESCRIPTION
+The script will get information about users from Azure Active Directory.
+
+.PARAMETER Filter
+Filters the AAD query based on the spesified parameters.
+
+.PARAMETER WhereObject
+Filters the returned results based on the spesified parameters.
+
+.PARAMETER Properties
+The properties to return from the search.
+
+.PARAMETER SortKey
+The sort key to use when sorting the results. By default, this is the first property selected.
+#>
+
+param(
+    [string]$Filter,
+    $Properties = @("UserPrincipalName", "DisplayName", "GivenName", "Surname", "UserType", "AccountEnabled", "PhysicalDeliveryOfficeName", "TelephoneNumber", "Mobile", "Mail", "MailNickName"),
+    $WhereObject = { $_.DirSyncEnabled -ne $true },
+    $SortKey = $Properties[0]
+)
+try { . (LoadDefaults -Invocation $MyInvocation) -Invocation $MyInvocation } catch { Write-Warning "Failed to load defaults. Is the module loaded?" }
+
+return Get-AzureADUser -Filter $Filter | Where-Object $WhereObject | Sort-Object $SortKey | Select-Object $Properties
