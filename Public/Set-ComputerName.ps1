d7300a66750ab2f335b779e91b168549f9e982b1
Jason Cook <JasonCook599@users.noreply.github.com>
2022-05-30 08:56:58 -0400

    Add support for renaming domain joined computers.

--- Public/Set-ComputerName.ps1
+++ Public/Set-ComputerName.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0.3
+.VERSION 1.0.4
 
 .GUID 0e319076-a254-46aa-948c-203373b9e47d
 
@@ -29,8 +29,6 @@
 
 #> 
 
-
-
 <#
 .DESCRIPTION
 This script will rename the computer based on the prefix and serial number.
@@ -49,12 +47,21 @@ The new name to use for the computer.
 #>
 
 [CmdletBinding(SupportsShouldProcess = $true)]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "Password")]
 param (
     [string]$Prefix,
+    [string]$User,
+    [string]$Password,
     [string]$Serial = (Get-WmiObject win32_bios).Serialnumber,
     $PrefixLenght = ($(15 - $Serial.length), $Prefix.Length | Measure-Object -Minimum ).Minimum,
     $NewName = $Prefix.Substring(0, $PrefixLenght) + $Serial
 )
 
+if ($User -and $Password) {
+    [SecureString]$SecurePassword = ($Password | ConvertTo-SecureString -AsPlainText -Force)
+    [pscredential]$Credentials = (New-Object System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword)
+    Write-Verbose "Renaming computer to `'$NewName`' as `'$User`'"
+    return Rename-Computer -NewName $NewName -DomainCredential $Credentials
+}
 Write-Verbose "Renaming computer to `'$NewName`'"
-Rename-Computer -NewName $NewName
+return Rename-Computer -NewName $NewName
