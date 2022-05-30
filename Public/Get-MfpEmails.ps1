6d32011a04bc1f8967f1f0515eb855a65f46d90f
Jason Cook <JasonCook599@users.noreply.github.com>
2022-05-30 08:35:31 -0400

    Rename

--- Public/Get-MfpEmails.ps1
+++ Public/Get-MfpEmails.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0.2
+.VERSION 1.0.5
 
 .GUID 9ee43161-d2de-4792-a59e-19ff0ef0717e
 
@@ -33,6 +33,12 @@
 
 
 
+
+
+
+
+
+
 <#
 .DESCRIPTION
 This script will output the email addresses needed for the scan to email function on MFPs.
@@ -51,7 +57,7 @@ How should the AD results be filtered?
 #>
 [CmdletBinding(SupportsShouldProcess = $true)]
 param(
-    [ValidateScript( { Test-Path ((Get-Item $_).parent) })][string]$Path = ".\AD.csv",
+    [ValidateScript( { Test-Path ((Get-Item $_).parent) })][string]$Path,
     [array]$Properties = ("name", "mail"),
     [string]$SearchBase ,
     [string]$Filter = "*"
@@ -64,6 +70,6 @@ $Result += Get-ADUser -Properties $Properties -Identity "koinonia"
 $Result += Get-ADUser -Properties $Properties -Identity "kcfit"
 $Result = $Result | Where-Object msExchHideFromAddressLists -ne $true | Select-Object name, mail | Sort-Object -Property Company, name
 $Result | ForEach-Object { $_.name = "$($_.name[0..23] -join '')" } #Trim lenght for import.
-$Result | Export-Csv -NoTypeInformation -Path $Path
+if ($Path) { $Result | Export-Csv -NoTypeInformation -Path $Path }
 Return $Result
 
