de0298a1931451d9a2c1ad7a0d7aeff6b2a9913d
Jason Cook <JasonCook599@users.noreply.github.com>
2022-06-29 15:53:34 -0400

    Add FortiClient support

---
+++ Public/Export-FortiClientConfig.ps1
@@ -0,0 +1,61 @@
+<#PSScriptInfo
+
+.VERSION 1.2.4
+
+.GUID 6604b9e8-5c58-4524-b094-07b549c2dad8
+
+.AUTHOR Jason Cook
+
+.COMPANYNAME ***REMOVED***
+
+.COPYRIGHT Copyright (c) ***REMOVED*** 2022
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#> 
+
+
+
+
+
+<#
+.DESCRIPTION
+This will export the current Forti Client configuration.
+
+.PARAMETER Path
+The location the configuration will be exported to.
+
+.EXAMPLE 
+Export-FortiClientConfig -Path backup.conf
+#>
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+    $Path = "backup.conf",
+    [ValidateScript( { Test-Path -Path $_ })]$FCConfig = 'C:\Program Files\Fortinet\FortiClient\FCConfig.exe',
+    $Password
+)
+try { . (LoadDefaults -Invocation $MyInvocation) -Invocation $MyInvocation } catch { Write-Warning "Failed to load defaults. Is the module loaded?" }
+
+
+$Arguments = ("-m all", ("-f " + $Path), "-o export", "-i 1")
+if ($Password) { $Arguments += "-p $Password" }
+
+if ($PSCmdlet.ShouldProcess($Path, "Export FortiClient Config")) {
+    Start-Process -FilePath $FCConfig -ArgumentList $Arguments -NoNewWindow -Wait    
+}
+
---
+++ Public/Import-FortiClientConfig.ps1
@@ -0,0 +1,64 @@
+<#PSScriptInfo
+
+.VERSION 1.2.4
+
+.GUID 309e82fe-9a41-4ba2-afb4-8ef85e0fe38d
+
+.AUTHOR Jason Cook
+
+.COMPANYNAME ***REMOVED***
+
+.COPYRIGHT Copyright (c) ***REMOVED*** 2022
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#> 
+
+
+
+
+
+<#
+.DESCRIPTION
+This will import the current Forti Client configuration.
+
+.PARAMETER Path
+The location the configuration will be imported from.
+
+.EXAMPLE 
+Import-FortiClientConfig -Path backup.conf
+
+.LINK
+https://getmodern.co.uk/automating-the-install-of-forticlient-vpn-via-mem-intune
+#>
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+    $Path = "backup.conf",
+    [ValidateScript( { Test-Path -Path $_ })]$FCConfig = 'C:\Program Files\Fortinet\FortiClient\FCConfig.exe',
+    $Password
+)
+try { . (LoadDefaults -Invocation $MyInvocation) -Invocation $MyInvocation } catch { Write-Warning "Failed to load defaults. Is the module loaded?" }
+
+
+$Arguments = ("-m all", ("-f " + $Path), "-o import", "-i 1")
+if ($Password) { $Arguments += "-p $Password" }
+
+if ($PSCmdlet.ShouldProcess($Path, "Import FortiClient Config")) {
+    Start-Process -FilePath $FCConfig -ArgumentList $Arguments -NoNewWindow -Wait    
+}
+
