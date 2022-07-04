64c008234d87bbb48dba534faf188cc12c8bfe17
Jason Cook <JasonCook599@users.noreply.github.com>
2022-07-04 12:53:28 -0400

    Add Search-Registry and Update-OfficeCache

---
+++ Public/Update-OfficeCache.ps1
@@ -0,0 +1,51 @@
+<#PSScriptInfo
+
+.VERSION 1.0.1
+
+.GUID 97314a7e-aba8-41e8-8b1d-ca81372ae070
+
+.AUTHOR Jason Cook
+
+.COMPANYNAME ***REMOVED***
+
+.COPYRIGHT Copyright (c) ***REMOVED*** 2022
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#> 
+
+
+
+<#
+.DESCRIPTION
+Update the office cache for each XML file in the current folder.
+#>
+
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+    $Path = (Get-ChildItem -Filter "*.xml"),
+    $Setup = ".\setup.exe"
+)
+
+try { . (LoadDefaults -Invocation $MyInvocation) -Invocation $MyInvocation } catch { Write-Warning "Failed to load defaults. Is the module loaded?" }
+
+$Path | ForEach-Object {
+    If ($PSCmdlet.ShouldProcess("$($_.Name)", "Update-OfficeCache")) {
+        Start-Process -FilePath $Setup -ArgumentList @("\download", $_.FullName) -NoNewWindow -Wait
+    }
+}
