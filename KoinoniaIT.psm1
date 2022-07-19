da04d7a97ed9a8cb79edcf5ff9f271c37dde0fd1
Jason Cook <JasonCook599@users.noreply.github.com>
2022-07-19 11:46:18 -0400

    Improve error handling and logging.

--- KoinoniaIT.psm1
+++ KoinoniaIT.psm1
@@ -281,7 +281,7 @@ If (!(Get-Module -ListAvailable -Name $Name)) {
 function LoadDefaults {
 <#PSScriptInfo
 
-.VERSION 1.0.2
+.VERSION 1.0.3
 
 .GUID 73e8a944-8951-4a89-9a54-d51db3f9afac
 
@@ -323,27 +323,28 @@ try {
     $ModuleName = (Get-Command -Name $Invocation.MyCommand -ErrorAction SilentlyContinue).ModuleName
     $ModulePath = (Get-Module -Name $ModuleName).Path
     $ModuleRoot = Split-Path -Parent -Path $ModulePath
-    Write-Verbose "Running command from a module."
+    Write-Debug "Running command from the `'$ModuleName`' module located at `'$ModulePath`'."
 }
-catch { Write-Verbose "Not running command from a module." }
+catch { Write-Debug "Not running command from a module." }
 
 try {
     $ScriptPath = ((Get-Item $Invocation.InvocationName -ErrorAction SilentlyContinue).DirectoryName)
     $ModuleRoot = Split-Path -Path $ScriptPath -Parent
     Test-Path -ErrorAction Stop -Path $ModuleRoot | Out-Null
-    Write-Verbose "Running command from a script."
+    Write-Debug "Running command from a script located at `'$ScriptPath`'."
 }
-catch { Write-Verbose "Could not find script parent." }
+catch { Write-Debug "Could not find script parent." }
 
 try {
     $DefaultsPath = Join-Path -Path $ModuleRoot -ChildPath $DefaultsScripts
-    Write-Verbose "Defaults Path: $DefaultsPath"
+    Write-Debug "Defaults Path is  `'$DefaultsPath`'"
     Test-Path -ErrorAction Stop -Path $DefaultsPath | Out-Null
     return $DefaultsPath
 }
-catch { Write-Error "Error loading defaults script." }
-
-Write-Error "Not running as a module and can't find script path. Defaults not loaded."
+catch {
+    Write-Debug "Error loading defaults script."
+    return Write-Error "Not running as a module and can't find script path. Defaults not loaded."
+}
 }
 function ParseGuid {
 <#PSScriptInfo
