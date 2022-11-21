de299d42261886efd6b943d2015c329c31030078
Jason Cook <JasonCook599@users.noreply.github.com>
2022-11-21 12:35:33 -0500

    Add parsing for Razer products

--- KoinoniaIT.psm1
+++ KoinoniaIT.psm1
@@ -4281,7 +4281,7 @@ return $Results
 function Get-NewComputerName {
 <#PSScriptInfo
 
-.VERSION 1.0.3
+.VERSION 1.0.4
 
 .GUID f0c0a88c-be5c-46ee-ab03-86272a36b5d7
 
@@ -4333,6 +4333,12 @@ param (
     [string]$Serial = (Get-WmiObject win32_bios).Serialnumber,
     $PrefixLenght = ($(15 - $Serial.length), $Prefix.Length | Measure-Object -Minimum ).Minimum
 )
+
+$Models = @{
+    Razer = "BY21\d{2}M(\d{8})"
+}
+
+$Models.Keys | ForEach-Object { if ($Serial -match $Models[$_]) { $Serial = $Matches[1] } }
 
 return $Prefix.Substring(0, $PrefixLenght) + $Serial
 }
--- Public/Get-NewComputerName.ps1
+++ Public/Get-NewComputerName.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0.3
+.VERSION 1.0.4
 
 .GUID f0c0a88c-be5c-46ee-ab03-86272a36b5d7
 
@@ -52,5 +52,11 @@ param (
     [string]$Serial = (Get-WmiObject win32_bios).Serialnumber,
     $PrefixLenght = ($(15 - $Serial.length), $Prefix.Length | Measure-Object -Minimum ).Minimum
 )
+
+$Models = @{
+    Razer = "BY21\d{2}M(\d{8})"
+}
+
+$Models.Keys | ForEach-Object { if ($Serial -match $Models[$_]) { $Serial = $Matches[1] } }
 
 return $Prefix.Substring(0, $PrefixLenght) + $Serial
