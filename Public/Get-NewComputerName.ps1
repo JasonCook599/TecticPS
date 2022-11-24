4ba1648e474711f201036a5691f141cf333b491b
JasonCook599 <JasonCook599@users.noreply.github.com>
2022-11-24 14:44:36 -0500

    Fix prefix length calculation

--- Public/Get-NewComputerName.ps1
+++ Public/Get-NewComputerName.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0.4
+.VERSION 1.0.6
 
 .GUID f0c0a88c-be5c-46ee-ab03-86272a36b5d7
 
@@ -50,13 +50,13 @@ The new name to use for the computer.
 param (
     [string]$Prefix,
     [string]$Serial = (Get-WmiObject win32_bios).Serialnumber,
-    $PrefixLenght = ($(15 - $Serial.length), $Prefix.Length | Measure-Object -Minimum ).Minimum
+    [int]$MaxLength = 15
 )
-
 $Models = @{
     Razer = "BY21\d{2}M(\d{8})"
 }
 
 $Models.Keys | ForEach-Object { if ($Serial -match $Models[$_]) { $Serial = $Matches[1] } }
 
+$PrefixLenght = ($($MaxLength - $Serial.length), $Prefix.Length | Measure-Object -Minimum ).Minimum
 return $Prefix.Substring(0, $PrefixLenght) + $Serial
