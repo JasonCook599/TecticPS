b14331d21903d329f3d56ab0a80902e5cd79c1ee
Jason Cook <JasonCook599@users.noreply.github.com>
2023-09-25 10:54:46 -0400

    Add try catch

--- KoinoniaITDefaults.ps1
+++ KoinoniaITDefaults.ps1
@@ -1,12 +1,14 @@
 param([Parameter(Mandatory = $true)] $Invocation)
-
-$Function = ([System.IO.Path]::GetFileNameWithoutExtension($Invocation.MyCommand.Name) -replace "-", "")
-Write-Verbose "Loading defaults for $Function"
+try {
+  $Function = ([System.IO.Path]::GetFileNameWithoutExtension($Invocation.MyCommand.Name) -replace "-", "")
+  Write-Verbose "Loading defaults for $Function"
 
 (Get-Command -Name ($Invocation.InvocationName)).Parameters.Keys | Foreach-Object {
-  $Default = $***REMOVED***It.$Function.$_
-  if (-not $Invocation.BoundParameters.Keys.contains($_) -and $Default) {
-    Write-Verbose "Setting `"$_`" to `"$Default`""
-    Set-Variable -Name $_ -Value $Default -WhatIf:$false
+    $Default = $***REMOVED***It.$Function.$_
+    if (-not $Invocation.BoundParameters.Keys.contains($_) -and $Default) {
+      Write-Verbose "Setting `"$_`" to `"$Default`""
+      Set-Variable -Name $_ -Value $Default -WhatIf:$false
+    }
   }
 }
+catch { Write-Warning "Failed to load defaults." }
